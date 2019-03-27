da91db5e0b3dc17528759d42374497e817055fdd
bizzzare <bizzzare7@gmail.com>
2019-03-27 18:52:34 +0200

    I'm done

--- DogsSocialNetwork/DogsSocialNetwork/Controllers/PaymentController.cs
+++ DogsSocialNetwork/DogsSocialNetwork/Controllers/PaymentController.cs
@@ -21,38 +21,39 @@ namespace DogsSocialNetwork.Controllers
         /// На цю сторінку LiqPay відправляє результат оплати. Вона вказана в data.result_url
         /// </summary>
         /// <returns></returns>
-        //[HttpPost]
+        [HttpPost]
         public ActionResult Redirect(int userId)
         {
             ViewBag.UserId = userId;
-            //ViewBag.ErrorDetails = "Something went wrong with payment";
+            ViewBag.ErrorDetails = "Something went wrong with payment";
 
-            //// --- Перетворюю відповідь LiqPay в Dictionary<string, string> для зручності:
-            //var request_dictionary = Request.Form.AllKeys.ToDictionary(key => key, key => Request.Form[key]);
+            // --- Перетворюю відповідь LiqPay в Dictionary<string, string> для зручності:
+            var request_dictionary = Request.Form.AllKeys.ToDictionary(key => key, key => Request.Form[key]);
 
-            //// --- Розшифровую параметр data відповіді LiqPay та перетворюю в Dictionary<string, string> для зручності:
-            //byte[] request_data = Convert.FromBase64String(request_dictionary["data"]);
-            //string decodedString = Encoding.UTF8.GetString(request_data);
-            //var request_data_dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(decodedString);
+            // --- Розшифровую параметр data відповіді LiqPay та перетворюю в Dictionary<string, string> для зручності:
+            byte[] request_data = Convert.FromBase64String(request_dictionary["data"]);
+            string decodedString = Encoding.UTF8.GetString(request_data);
+            var request_data_dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(decodedString);
 
-            //// --- Отримую сигнатуру для перевірки
-            //var mySignature = LiqPayHelper.GetLiqPaySignature(request_dictionary["data"]);
+            // --- Отримую сигнатуру для перевірки
+            var mySignature = LiqPayHelper.GetLiqPaySignature(request_dictionary["data"]);
 
-            //// --- Якщо сигнатура серевера не співпадає з сигнатурою відповіді LiqPay - щось пішло не так
-            //if (mySignature != request_dictionary["signature"])
-            //    return View("~/Views/Shared/Error.cshtml");
+            // --- Якщо сигнатура серевера не співпадає з сигнатурою відповіді LiqPay - щось пішло не так
+            if (mySignature != request_dictionary["signature"])
+                return View("~/Views/Shared/Error.cshtml");
 
 
-            //// --- Якщо статус відповіді "Тест" або "Успіх" - все добре
-            //if (request_data_dictionary["status"] == "sandbox" || request_data_dictionary["status"] == "success")
-            //{
-            //    // Тут можна оновити статус замовлення та зробити всі необхідні речі. Id замовлення можна взяти тут: request_data_dictionary[order_id]
-            //    // ...
+            // --- Якщо статус відповіді "Тест" або "Успіх" - все добре
+            if (request_data_dictionary["status"] == "sandbox" || request_data_dictionary["status"] == "success")
+            {
+                // Тут можна оновити статус замовлення та зробити всі необхідні речі. Id замовлення можна взяти тут: request_data_dictionary[order_id]
+                // ...
 
-            //    return View("~/Views/Shared/_Success.cshtml");
-            //}
+                return View("Thanks");
+            }
 
-            return View("Thanks");
+            return View("~/Views/Shared/Error.cshtml");
+
         }
     }
 }
